d8c71bed375c1cd815317d4fce40a6416c054d0f
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-08-02 22:42:58 +0530

    first commit

--- Backend/controllers/mongooseCntrl.js
+++ Backend/controllers/mongooseCntrl.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const connectDB =async  () =>{
 
     async function main() {
-        await mongoose.connect(process.env.MONGODB_URL);
-       //    await mongoose.connect('mongodb://localhost:27017/cakeWorld');
+        await mongoose.connect(process.env.MONGODB_URI);
    }
     main().then(()=>{
         console.log("Connected to database Succesfully!");
